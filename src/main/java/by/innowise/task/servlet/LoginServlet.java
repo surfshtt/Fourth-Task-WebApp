8a1eafa7d17754952cb477462f9f073acf325cb0
surfshtt <fk8ghtt@gmail.com>
2025-11-28 20:02:00 +0300

    Test

--- src/main/java/by/innowise/task/servlet/LoginServlet.java
+++ src/main/java/by/innowise/task/servlet/LoginServlet.java
@@ -2,6 +2,9 @@ package by.innowise.task.servlet;
 
 import java.io.*;
 
+import by.innowise.task.dao.impl.UserDao;
+import by.innowise.task.exception.DaoException;
+import by.innowise.task.model.user.UserModel;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 import org.apache.logging.log4j.Logger;
@@ -17,6 +20,25 @@ public class LoginServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
+
+        UserModel user = new UserModel();
+        user.setName("123");
+        user.setPassword("123");
+        user.setEmail("23@gmail.com");
+        user.setRole(UserModel.Role.ADMIN);
+        UserDao userDao = null;
+        try {
+            userDao = new UserDao();
+        } catch (DaoException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            userDao.insert(user);
+        } catch (DaoException e) {
+            throw new RuntimeException(e);
+        }
+
+        //response.sendRedirect("/");
     }
 
     public void destroy() {

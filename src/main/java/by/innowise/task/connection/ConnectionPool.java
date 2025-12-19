66707b357e2e5b59560e6bcd6356728483835761
surfshtt <fk8ghtt@gmail.com>
2025-12-19 13:21:45 +0300

    Added logging

--- src/main/java/by/innowise/task/connection/ConnectionPool.java
+++ src/main/java/by/innowise/task/connection/ConnectionPool.java
@@ -1,5 +1,8 @@
 package by.innowise.task.connection;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -7,6 +10,8 @@ import java.util.ArrayDeque;
 import java.util.Queue;
 
 public class ConnectionPool {
+
+    private static final Logger logger = LogManager.getLogger();
 
     private static final String URL = "jdbc:mysql://localhost:3306/web_project";
     private static final String USER = "root";
@@ -21,7 +26,9 @@ public class ConnectionPool {
     static {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
+            LogManager.getLogger().info("CONNECTION_POOL: JDBC driver successfully loaded");
         } catch (ClassNotFoundException e) {
+            LogManager.getLogger().error("CONNECTION_POOL: Failed to load JDBC driver", e);
             throw new ExceptionInInitializerError(e);
         }
     }
@@ -40,34 +47,45 @@ public class ConnectionPool {
                 availableConnections.add(connection);
             }
             catch (SQLException e) {
+                logger.error("CONNECTION_POOL: Failed to create connection {}", i + 1, e);
                 throw new ExceptionInInitializerError(e);
             }
         }
+        logger.info("CONNECTION_POOL: Initialized with {} connections", POOL_SIZE);
     }
 
     public synchronized Connection getConnection() throws SQLException {
         while (availableConnections.isEmpty()) {
             try {
+                logger.warn("CONNECTION_POOL: No available connections, waiting...");
                 wait();
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
+                logger.warn("CONNECTION_POOL: Waiting for connection was interrupted", e);
             }
         }
 
-        return availableConnections.poll();
+        Connection connection = availableConnections.poll();
+        logger.debug("CONNECTION_POOL: Connection acquired, available connections: {}", availableConnections.size());
+        return connection;
     }
 
     public synchronized void releaseConnection(Connection connection) {
         if (connection != null) {
             availableConnections.offer(connection);
             notifyAll();
+            logger.debug("CONNECTION_POOL: Connection released, available connections: {}", availableConnections.size());
+        } else {
+            logger.warn("CONNECTION_POOL: Attempted to release null connection");
         }
     }
 
     public synchronized void shutdown() throws SQLException {
+        logger.info("CONNECTION_POOL: Shutting down, closing {} connections", availableConnections.size());
         for (Connection conn : availableConnections) {
             conn.close();
         }
         availableConnections.clear();
+        logger.info("CONNECTION_POOL: Shutdown complete");
     }
 }
--- src/main/java/by/innowise/task/util/PasswordHasher.java
+++ src/main/java/by/innowise/task/util/PasswordHasher.java
@@ -1,19 +1,27 @@
 package by.innowise.task.util;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.mindrot.jbcrypt.BCrypt;
 
 public class PasswordHasher {
 
+    private static final Logger logger = LogManager.getLogger();
+
     private static final int WORKLOAD = 10;
 
     public static String hashPassword(String password) {
+        logger.debug("UTIL: Hashing password");
         return BCrypt.hashpw(password, BCrypt.gensalt(WORKLOAD));
     }
 
     public static boolean verifyPassword(String plainPassword, String storedHash) {
         try {
-            return BCrypt.checkpw(plainPassword, storedHash);
+            boolean result = BCrypt.checkpw(plainPassword, storedHash);
+            logger.debug("UTIL: Password verification result: {}", result);
+            return result;
         } catch (Exception e) {
+            logger.warn("UTIL: Password verification failed due to exception", e);
             return false;
         }
     }
